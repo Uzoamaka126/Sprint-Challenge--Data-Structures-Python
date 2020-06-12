027da02dc326c0a6cfe5ff2153b23bc5687d6fb6
Uzoamaka126 <vanessapeters126@gmail.com>
2020-06-12 10:44:15 +0100

    Complete first task

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,34 @@
+from linked_list import DoublyLinkedList
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # if storage is less than capacity
+        # then add new item to tail
+        # make last item the current
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
+        if self.storage.length == self.capacity:
+            self.current.value = item
+            if self.current is self.storage.tail:
+                self.current = self.storage.head
+            else:
+                self.current = self.current.next
+
+    def __len__(self):
+        return self.storage.length
 
     def get(self):
-        pass
+        list_buffer_contents = []
+        node = self.storage.head
+        while node is not None:
+            if node.value is not None:
+                list_buffer_contents.append(node.value)
+            node = node.next
+
+        return list_buffer_contents
